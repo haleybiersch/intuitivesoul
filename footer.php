de3bf071ba00814b9fb16bec4f0e2ad1237fe774
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-16 17:05:27 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php wp_footer();?>
+<?php include('/style.css');?>
+
+<footer>
+  <div>
+    <p class="u-text-1">Intuitive Soul</p>
+    <p class="u-text-2">Join Us.</p>
+    <div class="u-form">
+      <!--  email form -->
+      <form action="#" method="POST" class="u-inner-form u-clearfix">
+        <div class="u-form-group">
+          <label for="email" class="u-label-1">Email</label>
+          <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
+        </div>
+        <div class="u-form-group u-form-submit">
+          <a href="#" class="u-btn-submit">Subscribe</a>
+          <input type="submit" value="submit" class="u-form-control-hidden">
+        </div>
+      </form>
+    </div>
+    <small>&copy;2021 Intuitive Soul</small>
+  </div>
+</footer>
+
+
+</body>
+</html>
