1158f34b91343eec5f065d37457630f380a5f71b
Charlotte <charlotteamosborne@gmail.com>
2021-03-18 13:10:53 -0400

    updated archive page

--- archive.php
+++ archive.php
@@ -1,27 +1,27 @@
 <?php get_header();?>
 
-    <div class="container blog-page">
+    <div class="all-posts media-page">
         <div class="row">
-            <div class="col-12"><h1>Intuitive Soul Blog</h1></div>
+            <div class="col-12"><h1><?php the_title(); ?> <hr></h1></div>
         </div>
-        <div class="row featured">
-            <div class="col-12"><h2>Featured</h2></div>
-        <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-            <div class="col-md-4">
-                <div class="preview">
-                    <?php if(has_post_thumbnail()): ?>
-                        <a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url(); ?>"></a>
-                    <?php endif; ?>
-                    <div>
-                        <h3><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3>
-                        <a href="<?php the_permalink(); ?>" class="more btn btn-primary">Continue Reading</a>
+        <div class="latest-stories">
+            <div class="row">
+                <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card archives">
+                            <div class="card-body">
+                                <h3 class="card-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3><hr>
+                                <p><?php the_excerpt(); ?></p>
+                                <div class="button">
+                                    <a class="btn btn-primary" href="<?php the_permalink(); ?>">Listen Now</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                </div>
+                <?php endwhile; endif;?>
             </div>
-<?php endwhile; endif;?>
-
         </div>
+    </div>
 
 
 <?php get_footer(); ?>
