670b1365142fad5cf46bea17fc15920f9e00c964
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-19 10:19:27 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,21 +3,28 @@
 
 
 <footer>
-  <div class="container">
-    <p class="u-text-1">Intuitive Soul</p>
-    <p class="u-text-2">Join Us.</p>
-    <div class="u-form">
-      <!--  email form -->
-      <form action="#" method="POST" class="u-inner-form u-clearfix">
-        <div class="u-form-group">
-          <label for="email" class="u-label-1">Email</label>
-          <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
-        </div>
-        <div class="u-form-group u-form-submit">
-          <a href="#" class="u-btn-submit">Subscribe</a>
-          <input type="submit" value="submit" class="u-form-control-hidden">
-        </div>
-      </form>
+  <div class="row">
+    <div class="col-sm-6 px-5 py-5">
+      <p class="pfooter">If you enjoyed, were touched, and/or inspired<br>
+        by a show, please show your appreciation by donating<br>
+        a small token.</p><br>
+        <p class="pfooter">
+        <button class="u-btn-submit" type="submit">DONATE</button>
+      </p>
+    </div>
+    <div class="col-sm-6 px-5 py-5">
+      <p class="pfooter">Intuitive Soul</p>
+        <p class="pfooter">Join Us.</p>
+        <form action="#" method="POST" class="u-inner-form u-clearfix">
+          <div class="u-form-group">
+            <label for="email" class="u-label-1">Email</label>
+            <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
+          </div>
+          <div class="u-form-group u-form-submit">
+            <a href="#" class="u-btn-submit">Subscribe</a>
+            <input type="submit" value="submit" class="u-form-control-hidden">
+          </div>
+        </form>
     </div>
     <small>&copy;2021 Intuitive Soul</small>
   </div>
