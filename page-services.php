86338fe6527b79bf338397a008f4ab3a8638dd18
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-19 12:09:17 -0400

    Create page-services.php

---
+++ page-services.php
@@ -0,0 +1,53 @@
+<?php get_header();?>
+
+
+<div class="container py-4">
+	<h1><?php the_title();?></h1>
+</div>
+
+
+
+<div class="container">
+    <div class="row">
+      <h2>Intuitive Counseling</h2>
+      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <div class="row">
+      <h2>Energy Clearing</h2>
+    <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="row">
+      <h2>mBraining</h2>
+      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="row">
+      <h2>Meditation</h2>
+      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+
+
+
+
+<?php get_footer();?>
