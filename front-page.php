f5ecdc9c9afa19f8b9940da57bbda78d118c4493
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-19 20:47:11 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -37,55 +37,63 @@
 
       <section class="blog pb-5">
         <h2>FEATURED BLOGS</h2>
-            <div class="row">
-                <?php if ($featured_posts->have_posts()) : ?>
-    <?php while ($featured_posts->have_posts()) : $featured_posts->the_post(); ?>
-              <div class="col">
-                <div class="card h-100">
-                  <?php if(has_post_thumbnail()): ?>
-                      <a href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail(); ?></a>
-                  <?php endif; ?>
-                  <div class="card-body">
-                    <h5 class="card-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h5><hr>
-                    <p class="card-text"><?php the_excerpt(); ?></p>
-                      <div class="text-center"><a href="<?php the_permalink(); ?>" class="more btn btn-primary">Read More</a></div>
-                  </div>
-                </div>
-              </div>
-                    <?php endwhile; ?>
-                    <?php wp_reset_postdata(); ?>
-                <?php else : ?>
-                    <p><?php __('No News'); ?></p>
-                <?php endif; ?>
+        <div class="row featured-posts">
+            <?php if ($featured_posts->have_posts()) : ?>
+                <?php while ($featured_posts->have_posts()) : $featured_posts->the_post(); ?>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="preview card">
+                            <div class="card-body">
+                            <?php if(has_post_thumbnail()): ?>
+                                <a href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail(); ?></a>
+                            <?php endif; ?>
+                            <div class="text">
+                                <h3 class="card-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3><hr>
+                                <p><?php the_excerpt(); ?></p>
+                                <div class="text-center"><a href="<?php the_permalink(); ?>" class="more btn btn-primary">Continue Reading</a></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php __('No News'); ?></p>
+            <?php endif; ?>
         </div>
-
           </section>
 
           <section class="services pb-5">
             <h2>SERVICES</h2>
             <div class="container">
-              <div class="row">
-<?php if ($services->have_posts()) : ?>
-    <?php while ($services->have_posts()) : $services->the_post(); ?>
-                <div class="col-md-6 col-lg-4">
-                  <div class="card h-100">
-                    <?php if(has_post_thumbnail()): ?>
-                        <a href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail(); ?></a>
-                    <?php endif; ?>
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h5><hr>
-                      <p class="card-text"><?php the_excerpt(); ?></p>
-                        <div class="text-center"><a href="<?php the_permalink(); ?>" class="more btn btn-primary">Explore</a></div>
-                    </div>
-                  </div>
-                </div>
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
-<?php else : ?>
-    <p><?php __('No News'); ?></p>
-<?php endif; ?>
+
+              <div class="row services">
+                  <?php if ($services->have_posts()) : ?>
+                      <?php while ($services->have_posts()) : $services->the_post(); ?>
+                          <div class="col-md-6 col-lg-4">
+                              <div class="preview card">
+                                  <div class="card-body h-100">
+                                  <?php if(has_post_thumbnail()): ?>
+                                      <a href="<?php the_permalink(); ?>" class="thumbnail"><?php the_post_thumbnail(); ?></a>
+                                  <?php endif; ?>
+                                  <div class="text">
+                                      <h3 class="card-title"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3><hr>
+                                      <p><?php the_excerpt(); ?></p>
+                                      <div class="text-center"><a href="<?php the_permalink(); ?>" class="more btn btn-primary">Continue Reading</a></div>
+                                  </div>
+                              </div>
+                              </div>
+                          </div>
+                      <?php endwhile; ?>
+                      <?php wp_reset_postdata(); ?>
+                  <?php else : ?>
+                      <p><?php __('No News'); ?></p>
+                  <?php endif; ?>
               </div>
-              </div>
+              <br>
+
+              <a href="#" class="u-btn-submit">View All Services</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+
             </section>
 
 
