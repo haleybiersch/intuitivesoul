1e64ec2935d8b646f89618e0da2c0df332d45704
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-21 23:04:08 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,7 @@
         <a href=""><i class="fab fa-youtube"></i></a>
         <a href=""><i class="fab fa-facebook"></i></a>
         <a href=""><i class="fab fa-twitter"></i></a>
+        <a href=""><i class="fab fa-linkedin"></i></a>
     </div>
     <!-- End floating socal icons -->
 </aside>
