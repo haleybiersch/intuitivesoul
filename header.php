1cc09438c6d98ffa4b21ae4e944f063437dea03a
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-22 00:34:41 -0400

    Update header.php
    Fixed some stray tags

--- header.php
+++ header.php
@@ -1,45 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php wp_head();?>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Joseph Krogman, March 14 2021">
+	<meta name="description" content="Intuitive Soul">
+	<meta name="generator" content="Eclipse IDE, PHP module">
+    <?php wp_head(); // include CSS and includes ?>
+    <title>Intuitive Soul</title>
+
 </head>
 
 <body <?php body_class();?>>
-<a href="#main" class="skip-to-content-link">Skip to content</a>
 
-  <header >
-<div class="container">
-    <div class="search">
-    <form class="searchbox">
-        <input type="search" placeholder="Search..." name="search" class="searchbox-input" required>
-        <input type="submit" class="searchbox-submit">
-        <span class="searchbox-icon"><i class="fa fa-search" aria-hidden="true"></i></span>
-    </form>
-    </div>
-    <nav>
-        <h4 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h4>
-        <div class="nav-button">
-            <p class="d-none">Mobile Nav Button</p>
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-        </div>
-        <?php wp_nav_menu (
-            array(
+	<a href="#main" class="skip-to-content-link">Skip to content</a>
+
+	<header>
+		<div class="container">
+			<div class="search">
+				<form class="searchbox">
+					<input type="search" placeholder="Search..." name="search"
+						class="searchbox-input" required> <input type="submit"
+						class="searchbox-submit"> <span class="searchbox-icon"><i
+						class="fa fa-search" aria-hidden="true"></i></span>
+				</form>
+			</div>
+			<nav>
+				<h4 class="logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+				</h4>
+				<div class="nav-button">
+					<p class="d-none">Mobile Nav Button</p>
+					<div class="line1"></div>
+					<div class="line2"></div>
+					<div class="line3"></div>
+				</div>
+        <?php wp_nav_menu(array(
                 'theme_location' => 'top-menu',
-                'menu_class' => 'navigation'
-            )
+                'menu_class' => 'navigation')
+            );
+        ?>
 
-        ); ?>
+    		</nav>
+		</div>
 
-    </nav>
-  </div>
-
-  </header>
-<body>
-<div class="container">
-
-
+	</header>
