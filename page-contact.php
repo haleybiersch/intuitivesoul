94f792ec20afad6ce045ef7d75b57e844da936f5
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-21 23:20:44 -0400

    Update page-contact.php
    Added main/section, sidebar include

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,19 @@
 <?php /* Template Name: Test */ ?>
 <?php include 'header.php' ;?>
-<?php cn_include_content(44); ?>
+
+<main>
+
+	<!-- Main Content -->
+	<section class="main pt-5" id="main">
+    
+    <h1><?php single_cat_title() ?></h1>
+
+	<?php cn_include_content(44); ?>
+	
+	</section>
+
+    <?php include('sidebar.php') ?>
+
+</main>
+
 <?php include 'footer.php'; ?>
