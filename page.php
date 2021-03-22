92ea47bb06310f925288d4e38ba22e68ca1fe9e6
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-22 00:09:33 -0400

    Update page.php
    Fixed title

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 	<!-- Main Content -->
 	<section class="main pt-5" id="main">
     
-    <h1><?php single_cat_title() ?></h1>
+    <h1><?php the_title() ?></h1>
     
     <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_content(); ?>
