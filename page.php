b06ed3fa60138de2e432e7629795cffe09af9489
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-21 23:29:02 -0400

    Update page.php
    Added main & section tags, sidebar include

--- page.php
+++ page.php
@@ -1,10 +1,20 @@
 <?php @include('header.php'); ?>
-<div class="page">
-<h1 class="text-center"><?php the_title(); ?></h1>
 
-<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-    <?php the_content(); ?>
+<main>
 
-<?php endwhile; endif;?>
-</div>
+	<!-- Main Content -->
+	<section class="main pt-5" id="main">
+    
+    <h1><?php single_cat_title() ?></h1>
+    
+    <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+    
+    <?php endwhile; endif;?>
+
+	</section>
+
+    <?php include('sidebar.php') ?>
+
+</main>    
 <?php @include('footer.php'); ?>
