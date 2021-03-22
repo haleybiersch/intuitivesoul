2dbf1508a2193910ff0436d36e742b53c0fe4f9a
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-22 00:40:48 -0400

    Update page-about.php
    Added default container

--- page-about.php
+++ page-about.php
@@ -5,13 +5,13 @@
 	<section class="main pt-5" id="main">
     
     <h1><?php the_title() ?></h1>
-    
+    <div class="container">
 	<?php  if (have_posts()) : while(have_posts()) : the_post(); ?>
 
 		<?php the_content(); ?>
 
 	<?php  endwhile; endif;?>
-
+	</div>
 
 	</section>
 
