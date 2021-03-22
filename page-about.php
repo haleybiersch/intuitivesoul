21b376c61213b629f0de93dc9572418e996bac48
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-21 23:49:14 -0400

    Update page-about.php
    Moved content to the WP page

--- page-about.php
+++ page-about.php
@@ -6,55 +6,11 @@
     
     <h1><?php single_cat_title() ?></h1>
     
-    <div class="container mb-4">
-    	<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/images/angel&laurie3.jpg" alt="Portrait of Laurie" width="400" align="right"/>
-    </div>
-    <div class="container px-5 py-5">
-    	<h3><em>Background</em></h3>
-    	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-    		doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-    		veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-    		voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-    		magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-    		qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-    		numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-    		voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-    		suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
-    		iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-    		vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-    </p>
-    </div>
-    <div class="container px-5 py-5">
-    		<h3><em>My Journey</em></h3>
-    		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-    			doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-    			veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-    			ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-    			consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-    			porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-    			adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-    			dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-    			exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-    			consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit
-    			esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-    			voluptas nulla pariatur?
-    </p>
-    </div>
-    <div class="container px-5 py-5">
-    		<h3><em>Spiritual Beliefs</em></h3>
-    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-    	doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-    	veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-    	ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-    	consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
-    	porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-    	adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-    	dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-    	exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-    	consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit
-    	esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-    	voluptas nulla pariatur?</p>
-    		</div>
+	<?php  if (have_posts()) : while(have_posts()) : the_post(); ?>
+
+		<?php the_content(); ?>
+
+	<?php  endwhile; endif;?>
 
 
 	</section>
