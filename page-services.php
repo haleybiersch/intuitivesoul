a82fc228e6e0d33d60c976382218cb985dd55aad
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-21 23:27:44 -0400

    Update page-services.php
    Added main & section tags and sidebar include

--- page-services.php
+++ page-services.php
@@ -1,53 +1,59 @@
 <?php get_header();?>
 
 
-<div class="container py-4">
-	<h1><?php the_title();?></h1>
-</div>
+<main>
+
+	<!-- Main Content -->
+	<section class="main pt-5" id="main">
+    
+    <h1><?php single_cat_title() ?></h1>
 
 
-
-<div class="container">
-    <div class="row">
-      <h2>Intuitive Counseling</h2>
-      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="container">
+        <div class="row">
+          <h2>Intuitive Counseling</h2>
+          <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    
+        <div class="row">
+          <h2>Energy Clearing</h2>
+        <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="row">
+          <h2>mBraining</h2>
+          <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="row">
+          <h2>Meditation</h2>
+          <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     </div>
 
-    <div class="row">
-      <h2>Energy Clearing</h2>
-    <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div class="row">
-      <h2>mBraining</h2>
-      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div class="row">
-      <h2>Meditation</h2>
-      <p><img class="img-fluid" img src="<?php echo get_template_directory_uri(); ?>/images/candles.jpg" align="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-</div>
+	</section>
 
+    <?php include('sidebar.php') ?>
 
+</main>
 
 
 <?php get_footer();?>
