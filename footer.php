72f272d2a2885c501861c74535e923e38ad46e09
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-22 00:35:20 -0400

    Update footer.php
    Fix the full-wide footer

--- footer.php
+++ footer.php
@@ -1,26 +1,28 @@
 <?php wp_footer();?>
 
 <footer>
-  <div class="row">
-    <div class="col-sm-3 px-4 py-4">
-      <p class="pfooter">If you enjoyed, were touched, and/or inspired by a show, please show your appreciation by donating a small token.</p>
-      <p class="pfooter"><button class="u-btn-submit" type="submit">DONATE</button></p>
-    </div>
-    <div class="col-sm-6 px-8 py-8">
-      <p class="u-text-1">Intuitive Soul</p>
-    	<p class="u-text-2">Join Us.</p>
-        <form action="#" method="POST" class="u-inner-form u-clearfix">
-          <div class="u-form-group">
-            <label for="email" class="u-label-1">Email</label>
-            <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
-          </div>
-          <div class="u-form-group u-form-submit">
-            <a href="#" class="u-btn-submit">Subscribe</a>
-            <input type="submit" value="submit" class="u-form-control-hidden">
-          </div>
-        </form>
-    </div>
-    <small>&copy;<?php echo date("Y"); ?> Intuitive Soul</small>
+  <div class="container">
+      <div class="row">
+        <div class="col-sm-3 px-4 py-4">
+          <p class="pfooter">If you enjoyed, were touched, and/or inspired by a show, please show your appreciation by donating a small token.</p>
+          <p class="pfooter"><button class="u-btn-submit" type="submit">DONATE</button></p>
+        </div>
+        <div class="col-sm-6 px-8 py-8">
+          <p class="u-text-1">Intuitive Soul</p>
+        	<p class="u-text-2">Join Us.</p>
+            <form action="#" method="POST" class="u-inner-form u-clearfix">
+              <div class="u-form-group">
+                <label for="email" class="u-label-1">Email</label>
+                <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
+              </div>
+              <div class="u-form-group u-form-submit">
+                <a href="#" class="u-btn-submit">Subscribe</a>
+                <input type="submit" value="submit" class="u-form-control-hidden">
+              </div>
+            </form>
+        </div>
+        <small>&copy;<?php echo date("Y"); ?> Intuitive Soul</small>
+      </div>
   </div>
 </footer>
 
