500a7060b26245454a8f661254a0aeec829b8fc2
Charlotte <charlotteamosborne@gmail.com>
2021-03-25 15:53:33 -0400

    Added donate link to footer button

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="col-sm-3 px-4 py-4">
           <p class="pfooter">If you enjoyed, were touched, and/or inspired by a show, please show your appreciation by donating a small token.</p>
-          <p class="pfooter"><button class="u-btn-submit" type="submit">DONATE</button></p>
+          <a href="https://www.paypal.com/donate/?token=oX1R3YOVF1bZA64F4yN019BVinw1uGvjFbsZel4fKxVwTtU0np46ZySwT3Ya7CM3QGFxTW&country.x=CA&locale.x=CA" class="pfooter u-btn-submit" target="_blank">DONATE</a>
         </div>
         <div class="col-sm-6 px-8 py-8">
           <p class="u-text-1">Intuitive Soul</p>
