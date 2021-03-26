d9473e5104c7d7741bc4f9809776ade86daeab3e
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-26 12:00:24 -0400

    Update page-services.php

--- page-services.php
+++ page-services.php
@@ -5,10 +5,10 @@
 
 	<!-- Main Content -->
 	<section class="main pt-5" id="main">
-    
+
     <h1><?php the_title() ?></h1>
     <div class="container">
-	<?php  if (have_posts()) : while(have_posts()) : the_post(); ?>
+	<?php  if ($services->have_posts()) : while($services->have_posts()) : $services->the_post(); ?>
 
 		<?php the_content(); ?>
 
