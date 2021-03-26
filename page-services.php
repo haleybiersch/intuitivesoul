4445a8c23e826e4aa373cdd4b13fef5442f64793
Charlotte <charlotteamosborne@gmail.com>
2021-03-26 12:56:12 -0400

    quick outline for service page - NOT DONE YET

--- page-services.php
+++ page-services.php
@@ -6,18 +6,37 @@
 	<!-- Main Content -->
 	<section class="main pt-5" id="main">
 
-    <h1><?php the_title() ?></h1>
-    <div class="container">
-	<?php  if ($services->have_posts()) : while($services->have_posts()) : $services->the_post(); ?>
+        <div class="row services">
+            <?php if ($services->have_posts()) : ?>
+                <?php while ($services->have_posts()) : $services->the_post(); ?>
+                    <div class="col-md-8">
 
-		<?php the_content(); ?>
+                                <div class="text">
+                                    <h3>
+                                        <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a>
+                                    </h3>
 
-	<?php  endwhile; endif;?>
-	</div>
+                                    <p><?php the_excerpt(); ?></p>
+                                    <div class="text-center">
+                                        <a href="<?php the_permalink(); ?>" class="u-btn-submit">Continue Reading</a>
+                                    </div>
+                                </div>
+                    </div>
+                    <div class="col-md-4">
+                        <?php if(has_post_thumbnail()): ?>
+                            <a href="<?php the_permalink(); ?>"
+                               class="thumbnail"><?php the_post_thumbnail(); ?></a>
+                        <?php endif; ?>
+                    </div>
+                <div>
 
-	</section>
-
-    <?php include('sidebar.php') ?>
+                </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php __('No News'); ?></p>
+            <?php endif; ?>
+        </div>
 
 </main>
 
