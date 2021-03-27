4246a22762750db64ad4f4477b1b20931859c954
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-03-26 20:47:02 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,10 @@
 <aside>
     <!-- Floating Socal icons -->
     <div class="icon-bar">
-        <a href=""><i class="fab fa-youtube"></i></a>
-        <a href=""><i class="fab fa-facebook"></i></a>
-        <a href=""><i class="fab fa-twitter"></i></a>
-        <a href=""><i class="fab fa-linkedin"></i></a>
+        <a href="https://www.youtube.com/c/LaurieHustonIntuitiveSoul"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.facebook.com/IntuitiveSoulRadio><i class="fab fa-facebook"></i></a>
+        <a href="https://twitter.com/IntuitSoulRadio"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/in/lauriehuston/"><i class="fab fa-linkedin"></i></a>
     </div>
     <!-- End floating socal icons -->
 </aside>
