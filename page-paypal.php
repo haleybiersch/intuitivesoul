c231df6dc1845d29ddd1e93b99bbb7c451e1c27d
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-31 11:52:24 -0400

    Update page-paypal.php
    Formatted code

--- page-paypal.php
+++ page-paypal.php
@@ -8,129 +8,207 @@
     
     <h1><?php the_title() ?></h1>
     
-    <h2>Form Code</h2>
-<pre>&lt;form action="https://www.paypal.com/cgi-bin/webscr" method="post"&gt;
-    	&lt;input type="hidden" name="cmd" value="_s-xclick" /&gt;
-    	&lt;input type="hidden" name="hosted_button_id" value="INSERT_VALUE" /&gt;
-    	&lt;input type="submit" alt="Alt text" name="submit" value="INSERT_TEXT" class="u-btn-submit" /&gt;
-&lt;/form&gt;</pre>
-    
-    <h2>Output</h2>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-    	<input type="hidden" name="cmd" value="_s-xclick" />
-    	<input type="hidden" name="hosted_button_id" value="6795494" />
-    	<input type="submit" alt="PayPal Alt Text" name="submit" value="Donate Now" class="u-btn-submit" />
-    </form>
-    
-    <br />
-    <h2>Re-create PayPal Links</h2>
-    <p>Replace the "hosted_button_id" value and input submit value "Button Text" to add the PayPal option</p>
-    
+    <h2 class="pt-3"><a href="../intuitive-counseling/">Intuitive Counseling</a></h2>
     <table class="table table-responsive table-striped table-hover table-sm">
     	<tr>
-    		<td>hosted_button_id</td>
-    		<td>Service Offered</td>
-    		<td>Button Text</td>
-    	</tr>
-    	<tr>
-    		<td>6795494</td>
-    		<td>Donate</td>
-    		<td>Donate Now</td>
-    	</tr>
-    	<tr>
-    		<td>CMG2JN7ZALEX6</td>
     		<td>Quick 1 Question (answered via E-Mail ) $50</td>
-    		<td>1 Question</td>
+    		<td>
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="CMG2JN7ZALEX6" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>    		
+    		</td>
     	</tr>
     	<tr>
-    		<td>CTZ3AB7F9HSFQ</td>
     		<td>Full Intuitive Counseling Session (up to 5 questions answered via E-Mail) $100</td>
-    		<td>Full Intuitive Counseling Session</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="CTZ3AB7F9HSFQ" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>		    		
+    		</td>
     	</tr>
-    	<tr>
-    		<td>2HYUFC4X6EQ56</td>
+
+		<tr>
     		<td>Phone Intuitive Counseling Session $150 per Hour (this may also be done in-person)</td>
-    		<td>Phone Intuitive Counseling Session</td>
-    	</tr>
-    	<tr>
-    		<td>QQXT2UUCXDNU4</td>
-    		<td>Relationship Advice (up to 5 Questions answered via Email) $100</td>
-    		<td>Relationship Advice</td>
-    	</tr>
-    	<tr>
-    		<td>6C265LJHFJGJJ</td>
-    		<td>Relationship Advice (answered via Phone call, Skype or in-person) $150 per Hour</td>
-    		<td>Relationship Advice</td>
-    	</tr>
-    	<tr>
-    		<td>WVT8JHGYC462J</td>
-    		<td>Quan Yin Channeling (answered via E-Mail) $100</td>
-    		<td>Quan Yin Channeling</td>
-    	</tr>
-    	<tr>
-    		<td>ECNNUKYPGDYY8</td>
-    		<td>Quan Yin Channeling (Phone) $150</td>
-    		<td>Quan Yin Channeling</td>
-    	</tr>
-    	<tr>
-    		<td>6457600</td>
-    		<td>Quick Clearing (single issue/person/situation) $50 </td>
-    		<td>Quick Clearing</td>
-    	</tr>
-    	<tr>
-    		<td>6457609</td>
-    		<td>Quick Full Straight Clearing: $150</td>
-    		<td>Quick Full Straight Clearing</td>
-    	</tr>
-
-    	<tr>
-    		<td>6457621</td>
-    		<td>Quick Expanded Full Clearing: $200</td>
-    		<td>Quick Expanded Full Clearing</td>
-    	</tr>
-    	<tr>
-    		<td>6457640</td>
-    		<td>Full Personal Clearing: 2-3 session $400</td>
-    		<td>Full Personal Clearing</td>
-    	</tr>
-    	<tr>
-    		<td>6457657</td>
-    		<td>Full Personal Clearing: 1 session $300</td>
-    		<td>Full Personal Clearing</td>
-    	</tr>
-    	<tr>
-    		<td>6457680</td>
-    		<td>Rising Star Healing $150</td>
-    		<td>Rising Star Healing</td>
-    	</tr>
-    	<tr>
-    		<td>6457691</td>
-    		<td>Etheric Surgery: $50</td>
-    		<td>Etheric Surgery</td>
-    	</tr>
-    	<tr>
-    		<td>6457704</td>
-    		<td>DNA Activation: $100 – 22 Strand DNA Activation</td>
-    		<td>DNA Activation</td>
-    	</tr>
-    	<tr>
-    		<td>6457718</td>
-    		<td>Level 1 $495 ~ Psychic Self Defense</td>
-    		<td>Clearing Training Level 1</td>
-    	</tr>
-    	<tr>
-    		<td>6457734</td>
-    		<td>Advanced, Level 2 $495 ~ Clearing Work</td>
-    		<td>Clearing Training Level 2</td>
-    	</tr>
-    	<tr>
-    		<td>6457747</td>
-    		<td>Master, Level 3 $495 ~ Matrix Soul Retrieval</td>
-    		<td>Clearing Training Level 3</td>
-    	</tr>
-
-    </table>
+    		<td>
+	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="2HYUFC4X6EQ56" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form></td>
+		</tr>
+	</table>	
     
+	
+	<h2 class="pt-3"><a href="../energy-clearing/">Energy Clearing</a></h2>
+	<table class="table table-responsive table-striped table-hover table-sm">
+    	<tr>
+    		<td>Quick Clearing (single issue/person/situation) $50</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457600" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>Quick Full Straight Clearing: $150</td>
+    		<td>
+           <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457609" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+    		</td>
+    	</tr>
+		<tr>
+    		<td>Quick Expanded Full Clearing: $200</td>
+    		<td>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                	<input type="hidden" name="cmd" value="_s-xclick" />
+                	<input type="hidden" name="hosted_button_id" value="6457621" />
+                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+                </form>
+	        </td>
+		</tr>
+    	<tr>
+    		<td>Full Personal Clearing: 2-3 session $400</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457640" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>Full Personal Clearing: 1 session $300</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457657" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+    		</td>
+    	</tr>
+		<tr>
+    		<td>Level 1 $495 ~ Psychic Self Defense</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457718" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+	        </td>
+		</tr>
+		<tr>
+    		<td>Advanced, Level 2 $495 ~ Clearing Work</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457734" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+	        </td>
+		</tr>
+		<tr>
+    		<td>Master, Level 3 $495 ~ Matrix Soul Retrieval</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457747" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+	        </td>
+		</tr>
+	</table>	
+
+	
+	<h2 class="pt-3">Relationship Advice</h2>
+	<table class="table table-responsive table-striped table-hover table-sm">
+    	<tr>
+    		<td>Relationship Advice (up to 5 Questions answered via Email) $100</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="QQXT2UUCXDNU4" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>Relationship Advice (answered via Phone call, Skype or in-person) $150 per Hour</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6C265LJHFJGJJ" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+    		</td>
+    	</tr>
+    </table>	
+        
+    <h2 class="pt-3"><a href="../meditation/">Meditation</a></h2>
+	<table class="table table-responsive table-striped table-hover table-sm">
+    	<tr>
+    		<td>Quan Yin Channeling (answered via E-Mail) $100</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="WVT8JHGYC462J" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>	
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>Quan Yin Channeling (Phone) $150</td>
+    		<td>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="ECNNUKYPGDYY8" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>		
+    		</td>
+    	</tr>
+    </table>
+
+	 <h2 class="pt-3">Other Services Include</h2>
+	<table class="table table-responsive table-striped table-hover table-sm">
+    	<tr>
+    		<td>Rising Star Healing $150</td>
+    		<td>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                	<input type="hidden" name="cmd" value="_s-xclick" />
+                	<input type="hidden" name="hosted_button_id" value="6457680" />
+                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+                </form>		
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>Etheric Surgery: $50</td>
+    		<td>
+	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457691" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+    		</td>
+    	</tr>
+    	<tr>
+    		<td>DNA Activation: $100 - 22 Strand DNA Activation</td>
+    		<td>
+	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            	<input type="hidden" name="cmd" value="_s-xclick" />
+            	<input type="hidden" name="hosted_button_id" value="6457704" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            </form>
+    		</td>
+    	</tr>
+    </table>
     
     <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_content(); ?>
