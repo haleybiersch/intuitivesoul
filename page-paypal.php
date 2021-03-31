c1d1d0af68527844785e90cc789f83c97ce6a546
krogmanj <74281439+krogmanj@users.noreply.github.com>
2021-03-31 12:06:41 -0400

    Update page-paypal.php

--- page-paypal.php
+++ page-paypal.php
@@ -9,14 +9,14 @@
     <h1><?php the_title() ?></h1>
     
     <h2 class="pt-3"><a href="../intuitive-counseling/">Intuitive Counseling</a></h2>
-    <table class="table table-responsive table-striped table-hover table-sm">
+    <table class="table table-responsive table-striped table-hover table-sm purchase">
     	<tr>
     		<td>Quick 1 Question (answered via E-Mail ) $50</td>
     		<td>
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="CMG2JN7ZALEX6" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>    		
     		</td>
     	</tr>
@@ -26,7 +26,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="CTZ3AB7F9HSFQ" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>		    		
     		</td>
     	</tr>
@@ -37,21 +37,21 @@
 	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="2HYUFC4X6EQ56" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form></td>
 		</tr>
 	</table>	
     
 	
 	<h2 class="pt-3"><a href="../energy-clearing/">Energy Clearing</a></h2>
-	<table class="table table-responsive table-striped table-hover table-sm">
+	<table class="table table-responsive table-striped table-hover table-sm purchase">
     	<tr>
     		<td>Quick Clearing (single issue/person/situation) $50</td>
     		<td>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457600" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
     		</td>
     	</tr>
@@ -61,7 +61,7 @@
            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457609" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
     		</td>
     	</tr>
@@ -71,7 +71,7 @@
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 	<input type="hidden" name="cmd" value="_s-xclick" />
                 	<input type="hidden" name="hosted_button_id" value="6457621" />
-                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
                 </form>
 	        </td>
 		</tr>
@@ -81,7 +81,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457640" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
     		</td>
     	</tr>
@@ -91,7 +91,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457657" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
     		</td>
     	</tr>
@@ -101,7 +101,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457718" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
 	        </td>
 		</tr>
@@ -111,7 +111,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457734" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
 	        </td>
 		</tr>
@@ -121,7 +121,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457747" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
 	        </td>
 		</tr>
@@ -129,14 +129,14 @@
 
 	
 	<h2 class="pt-3">Relationship Advice</h2>
-	<table class="table table-responsive table-striped table-hover table-sm">
+	<table class="table table-responsive table-striped table-hover table-sm purchase">
     	<tr>
     		<td>Relationship Advice (up to 5 Questions answered via Email) $100</td>
     		<td>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="QQXT2UUCXDNU4" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
     		</td>
     	</tr>
@@ -146,21 +146,21 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6C265LJHFJGJJ" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
     		</td>
     	</tr>
     </table>	
         
     <h2 class="pt-3"><a href="../meditation/">Meditation</a></h2>
-	<table class="table table-responsive table-striped table-hover table-sm">
+	<table class="table table-responsive table-striped table-hover table-sm purchase">
     	<tr>
     		<td>Quan Yin Channeling (answered via E-Mail) $100</td>
     		<td>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="WVT8JHGYC462J" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>	
     		</td>
     	</tr>
@@ -170,21 +170,21 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="ECNNUKYPGDYY8" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>		
     		</td>
     	</tr>
     </table>
 
 	 <h2 class="pt-3">Other Services Include</h2>
-	<table class="table table-responsive table-striped table-hover table-sm">
+	<table class="table table-responsive table-striped table-hover table-sm purchase">
     	<tr>
     		<td>Rising Star Healing $150</td>
     		<td>
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 	<input type="hidden" name="cmd" value="_s-xclick" />
                 	<input type="hidden" name="hosted_button_id" value="6457680" />
-                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+                	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
                 </form>		
     		</td>
     	</tr>
@@ -194,7 +194,7 @@
 	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457691" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
     		</td>
     	</tr>
@@ -204,7 +204,7 @@
 	        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             	<input type="hidden" name="cmd" value="_s-xclick" />
             	<input type="hidden" name="hosted_button_id" value="6457704" />
-            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit" />
+            	<input type="submit" alt="PayPal Alt Text" name="submit" value="Add to Cart" class="u-btn-submit purchase" />
             </form>
     		</td>
     	</tr>
