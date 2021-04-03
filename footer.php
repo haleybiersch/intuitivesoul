f354fda4e343cf4d6f34e4517183e783f8b4d101
haleybiersch <72530382+haleybiersch@users.noreply.github.com>
2021-04-03 18:41:45 -0400

    mailchimp newsletter sign up

--- footer.php
+++ footer.php
@@ -14,16 +14,32 @@
         <div class="col-md-6 px-8 py-8">
           <p class="u-text-1">Intuitive Soul</p>
         	<p class="u-text-2">Join Us.</p>
-            <form action="#" method="POST" class="u-inner-form u-clearfix">
-              <div class="u-form-group">
-                <label for="email" class="u-label-1">Email</label>
-                <input type="email" class="u-input" placeholder="Email Address" id="email" name="email" required>
-              </div>
-              <div class="u-form-group u-form-submit">
-                <a href="#" class="u-btn-submit">Subscribe</a>
-                <input type="submit" value="submit" class="u-form-control-hidden">
-              </div>
-            </form>
+            <!-- Begin Mailchimp Signup Form -->
+<style type="text/css">
+	#mc_embed_signup{background:#000; clear:left; font:14px; color: #fff;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://Intuitivesoul.us9.list-manage.com/subscribe/post?u=39bcdfdbb8f7793099c55f10d&amp;id=ebe549107d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+<div class="mc-field-group text-center">
+	<label for="mce-EMAIL">Email Address:
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="u-btn-submit"></div>
+</div>
+
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_39bcdfdbb8f7793099c55f10d_ebe549107d" tabindex="-1" value=""></div>
+    </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
         </div>
         <small>&copy;<?php echo date("Y"); ?> Intuitive Soul</small>
       </div>
