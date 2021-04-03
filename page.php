1d81cdd79580884d1dd7409312668a30aa968c5b
Charlotte <charlotteamosborne@gmail.com>
2021-04-03 11:36:28 -0400

    Added title/featured image to pages/blogposts

--- page.php
+++ page.php
@@ -4,9 +4,11 @@
 
 	<!-- Main Content -->
 	<section class="main pt-5" id="main">
-    
+
     <h1><?php the_title() ?></h1>
-    
+        <?php if(has_post_thumbnail()): ?>
+            <div class="text-center featured-img"><?php the_post_thumbnail(); ?></div>
+        <?php endif; ?>
     <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_content(); ?>
     
--- single.php
+++ single.php
@@ -8,6 +8,10 @@
 
     <div class="blog-post">
         <div class="post">
+            <h1><?php the_title() ?></h1>
+            <?php if(has_post_thumbnail()): ?>
+            <div class="text-center featured-img"><?php the_post_thumbnail(); ?></div>
+            <?php endif; ?>
             <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
                 <?php the_content(); ?>
             <?php endwhile; endif;?>
